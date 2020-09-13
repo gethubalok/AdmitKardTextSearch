e6882ad8a6fbf5d465496bc1f896979ab3f3bec1
unknown <akmtanda97@gmail.com>
2020-09-13 21:55:20 +0530

    dfhjdf

--- index.js
+++ index.js
@@ -2,24 +2,15 @@ const express=require('express');
 const app=express();
 const port = process.env.PORT || 3000;
 const cookieparser=require('cookie-parser');
-
-
-
 const bodyParser=require('body-parser');
-
-
-
-
-
-
 const mongoose=require('mongoose');
 
 // connect to DB..
-mongoose.connect('mongodb+srv://alok:alok@cluster0.atba1.mongodb.net/Text?retryWrites=true&w=majority', // took data from .env file
-    { useUnifiedTopology: true,useNewUrlParser: true }, 
-()=>console.log('Connected to DB...')
-);
-// const db=require('./config/db');
+// mongoose.connect('mongodb+srv://alok:alok@cluster0.atba1.mongodb.net/Text?retryWrites=true&w=majority', // took data from .env file
+//     { useUnifiedTopology: true,useNewUrlParser: true }, 
+// ()=>console.log('Connected to DB...')
+// );
+const db=require('./config/db');
 
 // MiddleWares...
 app.use(express.static('./assets'));
